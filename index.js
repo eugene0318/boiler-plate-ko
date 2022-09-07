f68f062f61408b3379b0bd7b34ce084d557fb146
eugene0318 <nttott@gmail.com>
2022-09-08 01:22:49 +0900

    token 생성까지

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const port = 3000;
 const { User } = require("./models/User");
 const bodyParser = require("body-parser");
 const config = require("./config/key");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+
 //application/x-www-form-urlencoded분석
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json분석
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -33,5 +37,37 @@ app.post("/register", (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 email을 db에서 찾기
+  console.log(req.body);
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 비밀번호까지 맞다면 토큰을 형성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user.user_id });
+      });
+    });
   });
 });
